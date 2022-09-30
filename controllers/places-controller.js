650e557598c4e82667fb0c9363286833bbd071d6
yakir9970 <yakirafeka@gmail.com>
2022-09-30 15:33:39 +0300

    Added user routes

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,4 +1,4 @@
-const { uuid } = require("uuidv4");
+const { v4: uuidv4 } = require("uuid");
 
 const HttpError = require("../models/http-error");
 
@@ -52,23 +52,23 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid; //holds object: {uid:"u1"}
-  const place = DUMMY.find((p) => {
+  const places = DUMMY.filter((p) => {
     return p.creator === userId;
   });
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
-      new HttpError("No place found for user id:" + userId + "!", 404)
+      new HttpError("No places found for user id:" + userId + "!", 404)
     );
   }
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
   const { title, description, coordinates, address, creator } = req.body;
   const createdPlace = {
-    id: uuid(),
+    id: uuidv4(),
     title,
     description,
     location: coordinates,
@@ -103,7 +103,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
---
+++ controllers/users-controller.js
@@ -0,0 +1,57 @@
+const { v4: uuidv4 } = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY = [
+  {
+    id: "u1",
+    name: "max",
+    email: "test@test.com",
+    password: "testers",
+  },
+  {
+    id: "u2",
+    name: "maxy",
+    email: "test2@test.com",
+    password: "testers",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY.find((u) => u.email === email);
+
+  if (hasUser) {
+    return next(new HttpError("User Already Exists!", 422));
+  }
+
+  const createdUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password,
+  };
+  DUMMY.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    return next(new HttpError("Email or Password is Incorrect!", 404));
+  }
+
+  res.json({ message: "Logged In!" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
