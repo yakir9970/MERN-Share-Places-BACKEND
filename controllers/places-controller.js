2c23e7b22e2509de4d7862537475ca4bb6aa4bc2
yakir9970 <yakirafeka@gmail.com>
2022-10-02 20:31:52 +0300

    Finished places requests

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -8,45 +8,6 @@ const getCoordsForAddress = require("../util/location");
 
 const Place = require("../models/place");
 
-let DUMMY = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky acrapers in the world!",
-    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
-    address: "350 Fifth Avenue · Manhattan, New York 10118",
-    location: {
-      lat: 40.748441,
-      lng: -73.985664,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky acrapers in the world!",
-    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
-    address: "350 Fifth Avenue · Manhattan, New York 10118",
-    location: {
-      lat: 40.748441,
-      lng: -73.985664,
-    },
-    creator: "u2",
-  },
-  {
-    id: "p3",
-    title: "Empire State Building",
-    description: "One of the most famous sky acrapers in the world!",
-    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
-    address: "350 Fifth Avenue · Manhattan, New York 10118",
-    location: {
-      lat: 40.748441,
-      lng: -73.985664,
-    },
-    creator: "u1",
-  },
-];
-
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid; //holds object: {pid:"p1"}
   let place;
